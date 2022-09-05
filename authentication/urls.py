670959a9f4e354003fe6d2c6eb8e87fe136bf1e6
Milan Sony <milanfrom2001@gmail.com>
2022-09-05 22:54:16 +0530

    login and logout created

--- authentication/urls.py
+++ authentication/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
   path('',views.home, name="home"),
   path('signup',views.signup, name="signup"),
-  # path('login',views.login, name="login"),
-  # path('',views.signout, name="signout"),
+  path('login',views.login, name="login"),
+  path('logout',views.logout, name="logout"),
 ]
--- authentication/views.py
+++ authentication/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render,redirect
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User, auth
 from django.contrib import messages
 
 # Create your views here.
@@ -21,30 +21,37 @@ def signup(request):
     if pass1 == pass2:
       if User.objects.filter(username=uname).exists():
         messages.warning(request,"Username Already Exist")
+        return redirect('signup')
       elif User.objects.filter(email=email).exists():
         messages.warning(request,"Email Already Exist")
+        return redirect('signup')
       else:
         user = User.objects.create_user(username=uname,first_name=fname,last_name=lname,email=email,password=pass1)
         user.save()
         messages.success(request,"Your Account Has been Successfully Created")
-        return redirect('signup')
+        return redirect('login')
     else:
       messages.warning(request,"Password Is Not Matching")
+      return redirect('signup')
   return render(request,"signup.html")
 
-#signin page
-# def login(request):
-  # if request.method == 'POST':
-    # uname = request.POST['uname']
-    # pass1 = request.POST['pass1']
+#Login page
+def login(request):
+  if request.method == 'POST':
+    uname = request.POST['uname']
+    password = request.POST['password']
+    # https://docs.djangoproject.com/en/4.1/topics/auth/default/
+    user = auth.authenticate(username=uname,password=password) 
+    if user is not None:
+      auth.login(request,user)
+      return render(request,"userhomepage.html")
+    else:
+      messages.warning(request,"Invalid Credentials")
+      return redirect('login')
+  else:
+    return render(request,"login.html")
 
-    # User = authenticate(uname = uname, pass1 = pass1)
-
-    # if User is not None:
-      # login(request, User)
-      # return render(request,"userhomepage.html")
-    # else:
-      # messages.error(request,"Your Credentials Doesn't Match")
-      # return redirect('login')
-
-  # return render(request,"login.html")
+#logout
+def logout(request):
+  auth.logout(request)
+  return redirect('/')
