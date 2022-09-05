a3972468d3ae37dfb47a6225752279b8b8c46108
Milan Sony <milanfrom2001@gmail.com>
2022-09-05 14:18:30 +0530

    signup form created

--- authentication/urls.py
+++ authentication/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
   path('',views.home, name="home"),
   path('signup',views.signup, name="signup"),
-  path('signin',views.signin, name="signin"),
+  path('login',views.login, name="login"),
   # path('',views.signout, name="signout"),
 ]
--- authentication/views.py
+++ authentication/views.py
@@ -1,5 +1,6 @@
-from http.client import HTTPResponse
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth.models import User
+from django.contrib import messages
 
 # Create your views here.
 
@@ -9,8 +10,24 @@ def home(request):
 
 #signup page
 def signup(request):
+  if request.method == 'POST':
+    uname = request.POST['uname']
+    fname = request.POST['fname']
+    lname = request.POST['lname']
+    email = request.POST['email']
+    pass1 = request.POST['pass1']
+    pass2 = request.POST['pass2']
+
+    myusers = User.objects.create_user(uname, email, pass1)
+    myusers.first_name = fname
+    myusers.last_name = lname
+
+    myusers.save()
+    messages.success(request,"Your Account Has been Successfully Created")
+    return redirect('signup')
+
   return render(request,"signup.html")
 
 #signin page
-def signin(request):
-  return render(request,"signin.html")
+def login(request):
+  return render(request,"login.html")
